353eb48a11519b3ca7ca0174ebc7859caf631e70
tristanbagnulo <tristanbagnulo1@gmail.com>
2020-10-01 16:04:06 +1000

    First parts of assignment

--- app/src/main/java/au/edu/unsw/infs3634/numberguesser/ResultActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/numberguesser/ResultActivity.java
@@ -26,39 +26,47 @@ public class ResultActivity extends AppCompatActivity {
         //Declare and allocate variable to "result of guess" text box
         TextView myText = (TextView)findViewById(R.id.idResultText);
 
-        //Declare and allocate variable to "number of tries remaining" text bok
+        //Declare and allocate variable to "number of tries remaining" text box
         TextView guessesLeft = (TextView)findViewById(R.id.idGuessesRemaining);
-        if (numberUser == numberRandom) {
-            String myString = String.valueOf("Congratulations! You guessed it right");
-            myText.setText(myString);
-            //Set "number of tries remaining" text box text to nothing
-            String guessesLeftString = String.valueOf("");
-            guessesLeft.setText(guessesLeftString);
-        } else if (numberUser < numberRandom) {
-            String myString = String.valueOf("Incorrect! Try guessing a little higher");
-            myText.setText(myString);
 
-            //Get new number of tries and display it
-            String guessesLeftString = String.valueOf("You have "+(5-numberOfGuesses())+" guesses remaining");
-            guessesLeft.setText(guessesLeftString);
+        int guessNumber = numberOfGuesses();
+        if (guessNumber <= 5) {
+            if (numberUser == numberRandom) {
+                String myString = String.valueOf("Congratulations! You guessed it RIGHT!");
+                myText.setText(myString);
+                //Set "number of tries remaining" text box text to nothing
+                String guessesLeftString = String.valueOf("");
+                guessesLeft.setText(guessesLeftString);
+            } else if (numberUser < numberRandom) {
+                String myString = String.valueOf("Incorrect! You guessed too LOW.");
+                myText.setText(myString);
+                //Get new number of tries and display it
+                String guessesLeftString = String.valueOf("You have " + (5 - guessNumber) + " guesses remaining");
+                guessesLeft.setText(guessesLeftString);
+            } else {
+                String myString = String.valueOf("Incorrect! You guesses too HIGH.");
+                myText.setText(myString);
+                //Get new number of tries and display it
+                String guessesLeftString = String.valueOf("You have " + (5 - guessNumber) + " guesses remaining");
+                guessesLeft.setText(guessesLeftString);
+            }
         } else {
-            String myString = String.valueOf("Incorrect! Try guessing a little lower");
-            myText.setText(myString);
-
-            //Get new number of tries and display it
-            String guessesLeftString = String.valueOf("You have "+(5-numberOfGuesses())+" guesses remaining");
-            guessesLeft.setText(guessesLeftString);
+            //Launch the Termination screen
+            launchTerminateActivity();
         }
 
-        guessAgainButton = (Button) findViewById(R.id.idGuessAgainButton);
+    guessAgainButton = (Button) findViewById(R.id.idGuessAgainButton);
         guessAgainButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 finish();
             }
         });
+    }
 
-
-
+    private void launchTerminateActivity() {
+    //Launch it with an intent
+    Intent intent = new Intent(ResultActivity.this, TerminateActivity.class);
+    startActivity(intent);
     }
 
     private static int numberOfGuesses = 0;
